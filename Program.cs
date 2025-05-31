1494d29820b80f3fd3fe8ed35bd56d1620edad2d
GranicIvan <granic.ivan.01@gmail.com>
2025-05-31 16:37:49 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Hello, World!\n");
+
+        try {
+            foreach (string file in Directory.EnumerateFiles(args[0], "*.xml"))
+            {
+                string contents = File.ReadAllText(file);
+                changeCharacters(contents);
+            }
+        } catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString()); 
+        }
+        
+        Console.WriteLine("\nKraj");
+    }
+
+    static void changeCharacters(string contents) {
+        contents.Replace("\r\n", "\n");
+    }
+}
+
